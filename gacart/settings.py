14274f909eed0442d9fca870c9058eb1b3f58370
azadranjith <azadranjith@gmail.com>
2022-06-07 18:28:17 +0530

    new env gastyle

--- gacart/settings.py
+++ gacart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default = True,cast = bool) 
 
-ALLOWED_HOSTS = ['gaenv-env.eba-gbjmnqxy.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['gastyle.eba-2dtatxvk.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
